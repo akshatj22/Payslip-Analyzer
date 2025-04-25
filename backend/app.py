01bbf3c028d7222c12489c1c6393dcc23e2dd392
Akshat Jain <akshat22j@gmail.com>
2025-04-25 23:49:30 +0530

    domain changes

--- backend/app.py
+++ backend/app.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app, resources={
-    r"/upload": {"origins": "http://localhost:5173"},
+    r"/upload": {"origins": ["http://localhost:5173", "payslip-analyzer-ten.vercel.app"]},
     r"/ask": {
-        "origins": "http://localhost:5173",
+        "origins": ["http://localhost:5173", "payslip-analyzer-ten.vercel.app"],
         "methods": ["POST", "OPTIONS"],  # Explicitly allow OPTIONS
         "allow_headers": ["Content-Type"]
     }
